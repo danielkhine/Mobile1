c11bed43a1d4cd459259b276d670f64fc053564b
danielkhine <edlf20@gmail.com>
2024-03-16 14:15:11 -0500

    Rama 16Mar

--- app/src/main/java/com/example/mobile1/MainActivity.kt
+++ app/src/main/java/com/example/mobile1/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.mobile1
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mobile1.ui.theme.Mobile1Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +29,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             Mobile1Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
@@ -27,17 +41,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun Greeting(name: String) {
+    Column(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize()
+            .padding(10.dp),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.End
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(7.dp)
+        ) {
+            Text(
+                text = "Bienvenido $name!",
+                color = Color.Black,
+                fontSize = 20.sp
+            )
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text(
+                text = "Hola, $name!",
+                color = Color.White,
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .background(Color.Yellow)
+                    .padding(8.dp)
+            )
+        }
+
+        Text(
+            text = "Texto 2, $name!",
+            color = Color.Blue,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .background(Color.Yellow)
+                .padding(8.dp)
+        )
+
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     Mobile1Theme {
-        Greeting("Android")
+        Greeting("Mi Primera App")
     }
 }
